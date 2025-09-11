2e2293ce3bfbaa7cd6e5a2ce3d72ed324a105d34
you-mahha <revo1ute@yahoo.co.jp>
2025-09-11 23:22:47 +0900

    kadai005の再提出

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -2,14 +2,19 @@
 let userWeight;
 
 // 値の代入
-userWeight = '68'
+userWeight = 68;
 
 
 // 変数の宣言
 let userHeight;
 
 // 値の代入
-userHeight = '1.7'
+userHeight = 1.7;
+
+// コンソールへの出力
+console.log(userWeight);
+console.log(userHeight);
+
 
 // 四則演算
 console.log(userWeight / (userHeight * userHeight));
