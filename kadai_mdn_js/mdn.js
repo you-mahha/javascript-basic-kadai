1e3649acbbe73f03f12175d9e1f646cedb975380
you-mahha <revo1ute@yahoo.co.jp>
2025-09-15 08:58:37 +0900

    kadai_mdn_jsの再提出

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,4 +1,11 @@
-const event = new Date(2024, 10, 12,);
+const today = new Date();
+// 年代は() - 1と入力
+const fullyear = today.getFullYear() - 1;
 
-console.log(event.toDateString());
-// 予想される結果: "Wed Jul 28 1993"
+// 月は() + 1と入力
+const month = today.getMonth() + 1;
+
+const day = today.getDate();
+
+// 年月日の表示のため、`${要素}単位`を追加入力
+console.log(`${fullyear}年${month}月${day}日`);
